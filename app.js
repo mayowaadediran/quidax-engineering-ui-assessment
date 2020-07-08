a721d8dd1bfbe79b34af786f8d9bdef60f227065
Mayowa <mayowa@Mayowas-MacBook-Pro.local>
2020-07-08 03:05:08 +0100

    add flickity

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //variables
 
 const searchIcon = document.querySelector(".nav__search-icon");
+const booksList = document.querySelector(".bookshelf__list")
 
 
 //togglenav 
@@ -36,7 +37,65 @@ class Books {
 
 //show books
 class UI {
+
+    setupUI() {
+
+    }
+
+    displayBooks(books) {
+        let result = '';
+        books.forEach(book => {
+            result += `
+                <article class="book">
+                    <div class="book__thumbnail">
+                        <img
+                        src="${book.imageUrl}"
+                        />
+                    </div>
+                    <div class="book__details">
+                        <p class="book__details--status .book__details--status--available">
+                        Available
+                        </p>
+                        <h3 class="book__details--title">
+                        ${book.title}
+                        </h3>
+                        <p class="book__details--authors">
+                        ${book.author} - ${book.published}
+                        </p>
+                        <p class="book__details--category">
+                           ${book.genre}
+                        </p>
+                        <div class="book__details--review">
+                        <div class="book__details--rating">
+                            <p class="book__details--rating-text">
+                            Rating: ${book.ratings}.0
+                            </p>
+                            <div class="book__details--rating-stars">
+        
+                            </div>
+                    </div>
+                    <div class="book__details--share">
+                        <div class="people">
+                        
+                        </div>
+                        <div class="like">
+    
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </article>
+            `
+
+            booksList.innerHTML = result
+        })
+    }
+
+    displayRecentBooks() {
+
+    }
     //showfeatured
+
 
     //showRecent -- first 3
 
@@ -60,6 +119,7 @@ document.addEventListener("DOMContentLoaded", ()=> {
     //get all products
     books.getBooks()
       .then(books => {
+        ui.displayBooks(books)
        console.log(books)
       }).then(() => {
        
